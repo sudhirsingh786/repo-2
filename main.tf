9dbc4b21599af3706fdeaacf72eaab2935bad66c
sudhirsingh786 <123869147+sudhirsingh786@users.noreply.github.com>
2026-01-29 12:45:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ resource "google_service_account" "this" {
   display_name = "Service Account ${var.sa_name}"
   project      = var.project_id
 }
-
+output "service_account_email" {
+  value = google_service_account.this.email
+}
 
 module "labels" {
   source = "git::https://github.com/sudhirsingh786/repo-3.git?ref=bucket-label-test"
